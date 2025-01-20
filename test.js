e3f9008b2dcc87e902c60369e200c904cf69f1e4
Christian Attorri <christian.attorri@gsa.gov>
2025-01-20 11:00:24 -0500

    new thumbnail title

---
+++ test.js
@@ -0,0 +1,12 @@
+function shorten(s, max){
+    s = s.split(' ');
+    cut = '';
+    while (cut.length<=max && s.length){
+        cut += s.shift() + ' ';
+    }
+    if (s.length) cut = cut.substring(0,cut.length-1) + '...'
+    return cut
+}
+let s = "Readability: the readability of the title is so-so. Having a dash (-) and pipe (|)in the title tag decreases readability. On top of that, the title is truncated which hurts the readability as well. Relevant keywords: the title tag is not very relevant - what does “Canada’s Top Business Law Firm” have to do with your search query? Besides, the keyword usage in the title tag is not good - the applicable keywords are at the end"
+s = shorten(s,60)
+console.log(s);
