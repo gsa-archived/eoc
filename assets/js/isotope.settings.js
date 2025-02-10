d7b73bbe05bfb495b00b95a6dcc61380cea339bc
arnayv-47 <arnay.vohra@gsa.gov>
2025-02-10 12:54:19 -0500

    change filtering

--- assets/js/isotope.settings.js
+++ assets/js/isotope.settings.js
@@ -14,15 +14,7 @@ jQuery(document).ready(function ($) {
 
     if ($container !== undefined) {
 
-        let currentYear = new Date().getFullYear();
-        const archivedYears = 7;
-        const endYear = (currentYear - archivedYears);
-        let notArchivedYears = [];
-        for (let i = currentYear; i >= endYear; i--) {
-            notArchivedYears.push(`.${i}:not(.archived)`);
-        }
-    
-        let notArchivedFilter = notArchivedYears.join(", ");
+        let notArchivedFilter = ":not(.archived)";
 
         if (!location.hash && link.indexOf("/resources/") != -1) {
             location.hash = "archive_area=" + encodeURIComponent(notArchivedFilter);
@@ -149,29 +141,21 @@ jQuery(document).ready(function ($) {
           //combine all filters regardless of operator
           function buildFinalFilter(hashFilter) {
             let baseAND = combineAND([
-              hashFilter["resource"],
-              hashFilter["role"],
-              hashFilter["content"],
-              hashFilter["year"]
+                hashFilter["resource"],
+                hashFilter["role"],
+                hashFilter["content"],
+                hashFilter["year"] 
             ]);
+        
             let archiveVal = hashFilter["archive_area"];
-            if (!archiveVal || archiveVal === "*") {
-              return baseAND || "*";
-            }
-          
-            if (archiveVal.indexOf(",") > -1) {
-              let parts = archiveVal.split(",").map(p => p.trim());
-              let combinedParts = parts.map(p => {
-                if (!baseAND || baseAND === "*") return p;
-                return baseAND + p;
-              });
-
-              //join operators in the end
-              return combinedParts.join(", ");
+            if (archiveVal === ":not(.archived)") {
+                return baseAND ? baseAND + ":not(.archived)" : ":not(.archived)";
+            } else if (archiveVal === ".archived") {
+                return baseAND ? baseAND + ".archived" : ".archived";
             } else {
-              return baseAND ? baseAND + archiveVal : archiveVal;
+                return baseAND || "*";
             }
-          }
+        }
 
         function onHashChange() {
             // Current hash value
