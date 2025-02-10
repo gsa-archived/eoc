80d79c109fe4c68e1031d895b03a60bd76f7e297
arnayv-47 <arnay.vohra@gsa.gov>
2025-02-10 15:18:06 -0500

    historical isotope addition

--- assets/js/isotope.settings.js
+++ assets/js/isotope.settings.js
@@ -61,22 +61,21 @@ jQuery(document).ready(function ($) {
             // Current hash value
             var hashFilter = getHashFilter();
             // Set filters to current values (important for first run)
-
             if (link.indexOf("/resources/") != -1) {
-
                 filters["resource"] = hashFilter["resource"];
                 filters["role"] = hashFilter["role"];
                 filters["content"] = hashFilter["content"];
                 filters["year"] = hashFilter["year"];
+                filters["historical"] = hashFilter["historical"];
                 // data-filter attribute of clicked button
                 var currentFilter = $(this).attr("data-filter");
                 // Navigation group (subject or role) as object
                 var $navGroup = $(this).parents(".filter-list");
-
+        
                 // data-filter-group key for the current nav group
                 var filterGroup = $navGroup.attr("data-filter-group");
                 // If the current data-filter attribute matches the current filter,
-                if (currentFilter == hashFilter["resource"] || currentFilter == hashFilter["role"] || currentFilter == hashFilter["content"] || currentFilter == hashFilter["year"]) {
+                if (currentFilter == hashFilter["resource"] || currentFilter == hashFilter["role"] || currentFilter == hashFilter["content"] || currentFilter == hashFilter["year"] || currentFilter == ".historical") {
                     // Reset group filter as the user has unselected the button
                     filters[filterGroup] = "*";
                 } else {
@@ -84,19 +83,21 @@ jQuery(document).ready(function ($) {
                     filters[filterGroup] = $(this).attr("data-filter");
                 }
                 // Create new hash
-                // var newHash = "subject=" + encodeURIComponent( filters["subject"] ) + "&role=" + encodeURIComponent( filters["role"] ) + "&status=" + encodeURIComponent( filters["status"] );
                 var newHash = "resource=" + filters["resource"] + "&role=" + filters["role"] + "&content=" + filters["content"] + "&year=" + filters["year"];
+                if (filters["historical"] !== "*") {
+                    newHash += "&historical=" + encodeURIComponent(filters["historical"]);
+                }
                 // If sort value exists, add it to hash
                 if (sortValue) {
                     newHash = newHash + "&sort=" + encodeURIComponent(sortValue);
                 }
-
-                //Filter for News Page
+                // Apply the new hash to the URI, triggering onHashchange()
+                location.hash = newHash;
             } else if (link.indexOf("/news/") != -1) {
                 filters["content"] = hashFilter["content"];
                 // data-filter attribute of clicked button
                 var currentFilter = $(this).attr("data-filter");
-
+        
                 // Navigation group (subject or role) as object
                 var $navGroup = $(this).parents(".filter-list");
                 // data-filter-group key for the current nav group
@@ -110,15 +111,13 @@ jQuery(document).ready(function ($) {
                     filters[filterGroup] = $(this).attr("data-filter");
                 }
                 // Create new hash
-                // var newHash = "subject=" + encodeURIComponent( filters["subject"] ) + "&role=" + encodeURIComponent( filters["role"] ) + "&status=" + encodeURIComponent( filters["status"] );
                 var newHash = "content=" + filters["content"];
                 // If sort value exists, add it to hash
                 if (sortValue) {
                     newHash = newHash + "&sort=" + encodeURIComponent(sortValue);
                 }
+                location.hash = newHash;
             }
-            // Apply the new hash to the URI, triggering onHahschange()
-            location.hash = newHash;
         } // filterSelect
 
         function onHashChange() {
@@ -127,13 +126,16 @@ jQuery(document).ready(function ($) {
             // Concatenate subject and role for Isotope filtering
             if (link.indexOf("/resources/") != -1) {
                 var theFilter = hashFilter["resource"] + hashFilter["role"] + hashFilter["content"] + hashFilter["year"];
+                if (hashFilter["historical"] !== "*") {
+                    theFilter += ".historical";
+                }
                 if (hashFilter) {
                     // Repaint Isotope container with current filters and sorts
                     $container.isotope({
                         filter: decodeURIComponent(theFilter),
                         sortBy: hashFilter["sorts"]
                     });
-
+        
                     updateFilterCount();
                     // Toggle checked status of sort button
                     if (hashFilter["sorts"]) {
@@ -147,25 +149,23 @@ jQuery(document).ready(function ($) {
                     var roleFilters = hashFilter["role"].split(",");
                     var contentFilters = hashFilter["content"].split(",");
                     var yearFilters = hashFilter["year"].split(",");
-                    var allFilters = resourceFilters.concat(roleFilters);
-                    allFilters = allFilters.concat(contentFilters);
-                    allFilters = allFilters.concat(yearFilters);
+                    var historicalFilters = hashFilter["historical"] !== "*" ? [".historical"] : [];
+                    var allFilters = resourceFilters.concat(roleFilters, contentFilters, yearFilters, historicalFilters);
                     for (filter in allFilters) {
                         $(".filter-list").find("[data-filter='" + allFilters[filter] + "']").addClass("checked").attr("aria-checked", "true");
                     }
-                    // $( ".filter-list" ).find("[data-filter='" + hashFilter["subject"] + "'],[data-filter='" + hashFilter["role"] + "'] ,[data-filter='" + hashFilter["status"] + "']").addClass("checked").attr("aria-checked","true");
                 }
                 //News Page
             } else if (link.indexOf("/news/") != -1) {
                 var theFilter = hashFilter["content"];
-
+        
                 if (hashFilter) {
                     // Repaint Isotope container with current filters and sorts
                     $container.isotope({
                         filter: decodeURIComponent(theFilter),
                         sortBy: hashFilter["sorts"]
                     });
-
+        
                     updateFilterCount();
                     // Toggle checked status of sort button
                     if (hashFilter["sorts"]) {
@@ -179,11 +179,8 @@ jQuery(document).ready(function ($) {
                     for (filter in allFilters) {
                         $(".filter-list").find("[data-filter='" + allFilters[filter] + "']").addClass("checked").attr("aria-checked", "true");
                     }
-                    // $( ".filter-list" ).find("[data-filter='" + hashFilter["subject"] + "'],[data-filter='" + hashFilter["role"] + "'] ,[data-filter='" + hashFilter["status"] + "']").addClass("checked").attr("aria-checked","true");
                 }
-
             }
-
         } // onHahschange
 
         function getHashFilter() {
@@ -193,8 +190,9 @@ jQuery(document).ready(function ($) {
                 var role = location.hash.match(/role=([^&]+)/i);
                 var content = location.hash.match(/content=([^&]+)/i);
                 var year = location.hash.match(/year=([^&]+)/i);
+                var historical = location.hash.match(/historical=([^&]+)/i);
                 var sorts = location.hash.match(/sort=([^&]+)/i);
-
+        
                 // Set up a hashFilter array
                 var hashFilter = {};
                 // Populate array with matches and sorts using ternary logic
@@ -202,26 +200,24 @@ jQuery(document).ready(function ($) {
                 hashFilter["role"] = role ? role[1] : "*";
                 hashFilter["content"] = content ? content[1] : "*";
                 hashFilter["year"] = year ? year[1] : "*";
+                hashFilter["historical"] = historical ? historical[1] : "*";
                 hashFilter["sorts"] = sorts ? sorts[1] : "";
-
+        
                 return hashFilter;
             } else if (link.indexOf("/news/") != -1) {
                 // Get filters (matches) and sort order (sorts)
                 var content = location.hash.match(/content=([^&]+)/i);
                 var sorts = location.hash.match(/sort=([^&]+)/i);
-
+        
                 // Set up a hashFilter array
                 var hashFilter = {};
                 // Populate array with matches and sorts using ternary logic
                 hashFilter["content"] = content ? content[1] : "*";
                 hashFilter["sorts"] = sorts ? sorts[1] : "";
-
+        
                 return hashFilter;
-
             }
-
-
-        } // getHashFilter
+        }// getHashFilter
 
         // When the hash changes, run onHashchange
         window.onhashchange = onHashChange;
